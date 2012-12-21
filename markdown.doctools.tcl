b8af4ad374b0e2ef0c593aa3fd1ba865b8ae176c
Jim DeVona <jim.devona@gmail.com>
2012-12-21 02:04:15 -0500

    Sketched two-pass processing setup. Read document info commands and ignore text during first pass; output cached document info and process text during second pass.

--- markdown.doctools.tcl
+++ markdown.doctools.tcl
@@ -3,7 +3,7 @@
 #
 
 proc fmt_numpasses {} {
-	return 1
+	return 2
 }
 
 proc fmt_listvariables {} {
@@ -12,16 +12,17 @@ proc fmt_listvariables {} {
 
 proc fmt_varset {varname value} {
 	# no module configuration variables supported
+	# could provide options to toggle behavior between GitHub flavor and vanilla
 	error "unknown variable $varname"
 }
 
 proc fmt_initialize {} {
-	# start of a conversion run
+	global docinfo
+	set docinfo [dict create category {} copyright {} keywords {} moddesc {} require {} see_also {} titledesc {}]
 }
 
 proc fmt_setup {pass} {
-	# start of specified conversion pass
-	# (at least one pass per run)
+	switch $pass 1 mddt_setup_1 2 mddt_setup_2
 }
 
 proc fmt_postprocess {text} {
@@ -29,19 +30,270 @@ proc fmt_postprocess {text} {
 }
 
 proc fmt_shutdown {} {
-	# end of conversion run
+	global docinfo
+	dict unset category copyright keywords moddesc require see_also titledesc
 }
 
-# 22 text markup cmds
-# 20 text structure commands 
-# 7 document info commands
-
 #
-# FORMATTING COMMANDS
+# PASS SETUP
 #
 
-proc fmt_plain_text {text} {
-	return $text
+proc mddt_setup_1 {} {
+	
+	#
+	# Document information commands active
+	#
+	
+	# name of document category
+	proc fmt_category {text} {
+		global docinfo
+		# "If this command is used multiple times the last value specified is used."
+		dict set docinfo category $text
+	}
+
+	# document copyright
+	proc fmt_copyright {text} {
+		global docinfo
+		# "When invoked more than once the assignments are accumulated."
+		dict lappend docinfo copyright $text
+	}
+	
+	# document keyword list
+	proc fmt_keywords {args} {
+		global docinfo
+		# "If this command is used multiple times al the arguments accumulate."
+		dict lappend docinfo keywords {*}$args
+	}
+
+	# document module description
+	proc fmt_moddesc {text} {
+		global docinfo
+		dict set docinfo moddesc $text
+		# titledesc "defaults to the value registered by moddesc."
+		if {[dict get $docinfo titledesc] eq {}} {
+			dict set docinfo titledesc $text
+		}
+	}
+
+	# package requirement of document module
+	proc fmt_require {package {version {}}} {
+		global docinfo
+		dict lappend docinfo require [list $package $version]
+	}
+
+	# cross-references to other documents
+	proc fmt_see_also {args} {
+		global docinfo
+		# "If this command is used multiple times all the arguments accumulate."
+		dict lappend docinfo see_also {*}$args
+	}
+
+	# document description (defaults to fmt_moddesc)
+	proc fmt_titledesc {text} {
+		global docinfo
+		dict set docinfo titledesc $text
+	}
+	
+	#
+	# Text structure and markup commands ignored during first pass.
+	#
+	
+	proc fmt_plain_text {text} {}
+	# ... plus text structure commands ...
+	proc fmt_arg {text} {}
+	proc fmt_class {text} {}
+	proc fmt_cmd {text} {}
+	proc fmt_const {text} {}
+	proc fmt_emph {text} {}
+	proc fmt_file {text} {}
+	proc fmt_fun {text} {}
+	proc fmt_image {id {label {}}} {}
+	proc fmt_method {text} {}
+	proc fmt_namespace {text} {}
+	proc fmt_opt {text} {}
+	proc fmt_option {text} {}
+	proc fmt_package {text} {}
+	proc fmt_sectref {id {label {}}} {}
+	proc fmt_sectref-external {label} {}
+	proc fmt_syscmd {text} {}
+	proc fmt_term {text} {}
+	proc fmt_type {text} {}
+	proc fmt_uri {uri {label {}}} {}
+	proc fmt_usage {args} {}
+	proc fmt_var {text} {}
+	proc fmt_widget {text} {}
+}
+
+proc mddt_setup_2 {} {
+
+	#
+	# Document information commands ignored during second pass.
+	#
+	
+	proc fmt_category {text} {}
+	proc fmt_copyright {text} {}
+	proc fmt_keywords {args} {}
+	proc fmt_moddesc {text} {}
+	proc fmt_require {package {version {}}} {}
+	proc fmt_see_also {args} {}
+	proc fmt_titledesc {text} {}
+	
+	#
+	# Text structure and markup commands active
+	#
+	
+	# plain text with no markup
+	proc fmt_plain_text {text} {
+		return $text
+	}
+	
+	# ... plus text structure commands ...
+	
+	# name of a command argument
+	proc fmt_arg {text} {
+		# em
+		return "*$text*"
+	}
+
+	# name of a class
+	proc fmt_class {text} {
+		# code
+		return "`$text`"
+	}
+
+	# name of a Tcl command
+	proc fmt_cmd {text} {
+		# code
+		return "`$text`"
+	}
+
+	# value of a constant
+	proc fmt_const {text} {
+		# code
+		return "`$text`"
+	}
+
+	# generic emphasis
+	proc fmt_emph {text} {
+		# strong
+		return "**$text**"
+	}
+
+	# name of a file or directory (a path)
+	proc fmt_file {text} {
+		# quoted code
+		return "\"`$text`\""
+	}
+
+	# name of a function
+	proc fmt_fun {text} {
+		# code
+		return "`$text`"
+	}
+
+	# image id (URI) and possibly label
+	proc fmt_image {id {label {}}} {
+		if {$label eq {}} {
+			set label $id
+		}
+		# embedded img
+		return "!\[$label\]($id)"
+	}
+
+	# name of an object method
+	proc fmt_method {text} {
+		# code
+		return "`$text`"
+	}
+
+	# name of a namespace
+	proc fmt_namespace {text} {
+		# code
+		return "`$text`"
+	}
+
+	# optional, as command arguments
+	proc fmt_opt {text} {
+		# quoted with ?
+		return "?$text?"
+	}
+
+	# name of an option (ie, a command switch)
+	proc fmt_option {text} {
+		# code
+		return "`$text`"
+	}
+
+	# name of package
+	proc fmt_package {text} {
+		# code
+		return "`$text`"
+	}
+
+	# reference to section id, possibly with label
+	proc fmt_sectref {id {label {}}} {
+		if {$label eq {}} {
+			set label $id
+		}
+		# strong link
+		# (should link to id, using GitHub's anchor form of id)
+		return "**$label**"
+	}
+
+	# reference to external section with label
+	proc fmt_sectref-external {label} {
+		# strong
+		return "**$label**"
+	}
+
+	# name of an external command
+	proc fmt_syscmd {text} {
+		# code
+		return "`$text`"
+	}
+
+	# general terminology
+	proc fmt_term {text} {
+		# em
+		return "*$text*"
+	}
+
+	# name of a data type
+	proc fmt_type {text} {
+		# code
+		return "`$text`"
+	}
+
+	# URI and possibly its label
+	proc fmt_uri {uri {label {}}} {
+		if {$label eq {}} {
+			set label $uri
+		}
+		# link
+		return "\[$label\]($uri)"
+	}
+
+	# syntax of a command (first arg) with arguments (remainder of args)
+	# like call, but silent; used only to populate synopsis
+	# possibly fmt_call can use fmt_usage, plus list management stuff
+	proc fmt_usage {args} {
+		# presumably the doctools frontend knows to keep quiet and insert in synopsis?
+		# args are presumably already formatted with appropriate cmd, arg, etc commands.
+		return [join $args]
+	}
+
+	# name of a variable
+	proc fmt_var {text} {
+		# code
+		return "`$text`"
+	}
+
+	# name of a widget
+	proc fmt_widget {text} {
+		# code
+		return "`$text`"
+	}
+	
 }
 
 # Document Info
@@ -56,179 +308,3 @@ proc fmt_plain_text {text} {
 # Second pass: these document info procs are defined to be no-ops; all others
 # perform as usual. Insert cached document info values as appropriate - into
 # manpage_begin (header), manpage_end (footer), or at beginning of description.
-
-# name of document category
-proc fmt_category {text} {
-}
-
-# document copyright
-proc fmt_copyright {text} {
-}
-
-# document keyword list
-proc fmt_keywords {args} {
-}
-
-# document module description
-proc fmt_moddesc {text} {
-}
-
-# package requirement of document module
-proc fmt_require {package {version {}}} {
-}
-
-# cross-references to other documents
-proc fmt_see_also {args} {
-}
-
-# document description (defaults to fmt_moddesc)
-proc fmt_titledesc {text} {
-}
-
-
-# Text Markup
-
-# name a command argument
-proc fmt_arg {text} {
-	# em
-	return "*$text*"
-}
-
-# name of a class
-proc fmt_class {text} {
-	# code
-	return "`$text`"
-}
-
-# name of a Tcl command
-proc fmt_cmd {text} {
-	# code
-	return "`$text`"
-}
-
-# value of a constant
-proc fmt_const {text} {
-	# code
-	return "`$text`"
-}
-
-# generic emphasis
-proc fmt_emph {text} {
-	# strong
-	return "**$text**"
-}
-
-# name of a file or directory (a path)
-proc fmt_file {text} {
-	# quoted code
-	return "\"`$text`\""
-}
-
-# name of a function
-proc fmt_fun {text} {
-	# code
-	return "`$text`"
-}
-
-# image id (URI) and possibly label
-proc fmt_image {id {label {}}} {
-	if {$label eq {}} {
-		set label $id
-	}
-	# embedded img
-	return "!\[$label\]($id)"
-}
-
-# name of an object method
-proc fmt_method {text} {
-	# code
-	return "`$text`"
-}
-
-# name of a namespace
-proc fmt_namespace {text} {
-	# code
-	return "`$text`"
-}
-
-# optional, as command arguments
-proc fmt_opt {text} {
-	# quoted with ?
-	return "?$text?"
-}
-
-# name of an option (ie, a command switch)
-proc fmt_option {text} {
-	# code
-	return "`$text`"
-}
-
-# name of package
-proc fmt_package {text} {
-	# code
-	return "`$text`"
-}
-
-# reference to section id, possibly with label
-proc fmt_sectref {id {label {}}} {
-	if {$label eq {}} {
-		set label $id
-	}
-	# strong link
-	# (should link to id, using GitHub's anchor form of id)
-	return "**$label**"
-}
-
-# reference to external section with label
-proc fmt_sectref-external {label} {
-	# strong
-	return "**$label**"
-}
-
-# name of an external command
-proc fmt_syscmd {text} {
-	# code
-	return "`$text`"
-}
-
-# general terminology
-proc fmt_term {text} {
-	# em
-	return "*$text*"
-}
-
-# name of a data type
-proc fmt_type {text} {
-	# code
-	return "`$text`"
-}
-
-# URI and possibly its label
-proc fmt_uri {uri {label {}}} {
-	if {$label eq {}} {
-		set label $uri
-	}
-	# link
-	return "\[$label\]($uri)"
-}
-
-# syntax of a command (first arg) with arguments (remainder of args)
-# like call, but silent; used only to populate synopsis
-# possibly fmt_call can use fmt_usage, plus list management stuff
-proc fmt_usage {args} {
-	# presumably the doctools frontend knows to keep quiet and insert in synopsis?
-	# args are presumably already formatted with appropriate cmd, arg, etc commands.
-	return [join $args]
-}
-
-# name of a variable
-proc fmt_var {text} {
-	# code
-	return "`$text`"
-}
-
-# name of a widget
-proc fmt_widget {text} {
-	# code
-	return "`$text`"
-}
