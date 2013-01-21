fc5c4eb9a8ab4cd3020a966e7a7cd2ad5d74c4c5
Jim DeVona <jim.devona@gmail.com>
2013-01-20 19:08:01 -0500

    tidy up markup tests

--- tests/markup.test.tcl
+++ tests/markup.test.tcl
@@ -17,13 +17,11 @@ proc mdh {content} {
 
 proc mtest {description dtinput mdoutput} {
 	global testnum
-	test markup-[incr testnum] $description -setup {
-		::doctools::new doc -format ../markdown.doctools.tcl
-	} -body {
-		doc format [dth $dtinput]
-	} -cleanup {
-		doc destroy
-	} -result [mdh $mdoutput]
+	test markup-[incr testnum] $description \
+			-setup {::doctools::new doc -format ../markdown.doctools.tcl}
+			-body {doc format [dth $dtinput]}
+			-cleanup {doc destroy}
+			-result [mdh $mdoutput]
 }
 
 mtest "arg - em"         {[arg foo]}       {*foo*}
@@ -34,20 +32,20 @@ mtest "emph - strong"    {[emph foo]}      {**foo**}
 mtest "file - \"code\""  {[file foo]}      {"`foo`"}
 mtest "fun - code"       {[fun foo]}       {`foo`}
 mtest "image"            {[image foo]}     {![foo](foo)}
-mtest "image with label" {[image foo bar]} {![bar](foo)}
+mtest "image w/label"    {[image foo bar]} {![bar](foo)}
 mtest "method - code"    {[method foo]}    {`foo`}
 mtest "namespace - code" {[namespace foo]} {`foo`}
 mtest "opt - ?opt?"      {[opt foo]}       {?foo?}
 mtest "option - code"    {[option foo]}    {`foo`}
 mtest "package - code"   {[package foo]}   {`foo`}
 mtest "sectref"          {[sectref foo]}   {**foo**}
-mtest "sectref w/label"  {[sectref foo bar]} {**bar**}
+mtest "sectref w/label"  {[sectref a b]}   {**b**}
 mtest "sectref-external" {[sectref-external foo]} {**foo**}
 mtest "syscmd - code"    {[syscmd foo]}    {`foo`}
 mtest "term - em"        {[term foo]}      {*foo*}
 mtest "type - code"      {[type foo]}      {`foo`}
 mtest "uri"              {[uri foo]}       {[foo](foo)}
-mtest "uri with label"   {[uri foo bar]}   {[bar](foo)}
+mtest "uri w/label"      {[uri foo bar]}   {[bar](foo)}
 mtest "var - code"       {[var foo]}       {`foo`}
 mtest "widget - code"    {[widget foo]}    {`foo`}
 
